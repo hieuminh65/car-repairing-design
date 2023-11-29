fdc60de5a323447a243226aa8e931534b8e0577f
hieuminh65 <hieuminhnguyen@usf.edu>
2023-11-29 09:59:16 -0500

    fix config

--- config.py
+++ config.py
@@ -4,12 +4,8 @@ import os
 load_dotenv()
 
 class Config:
-    # HOST = os.getenv('HOST')
-    # USER = os.getenv('USER_DB')
-    # DATABASE = os.getenv('DATABASE')
-    # PASSWORD = os.getenv('PASSWORD')
-    HOST = "localhost"
-    USER = "postgres"
-    DATABASE = "final_project"
-    PASSWORD = "Hieu652003psql"
+    HOST = os.getenv('HOST')
+    USER = os.getenv('USER_DB')
+    DATABASE = os.getenv('DATABASE')
+    PASSWORD = os.getenv('PASSWORD')
     SECRET_KEY = os.getenv('SECRET_KEY')
