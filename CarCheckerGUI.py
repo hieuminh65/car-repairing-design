d79e39f0ff705491452217f6655f43357e499f3b
hieuminh65 <hieuminhnguyen@usf.edu>
2023-12-01 13:30:00 -0500

    fix up to toan code

--- CarCheckerGUI.py
+++ CarCheckerGUI.py
@@ -21,7 +21,6 @@ def CarCheckerMain():
 
     st.title("Cars Waitlist")
     
-    # Fetch and display data
     try:
         with connection.cursor() as cursor:
             cursor.execute("SELECT * FROM carunchecked WHERE uncheck = TRUE;")
@@ -31,10 +30,8 @@ def CarCheckerMain():
         st.error(f"Error: Unable to fetch data from the database. {e}")
         st.stop()
 
-    # Creating a dataframe from the fetched data
     df = pd.DataFrame(data, columns=colnames)
 
-    # Drop the 'Uncheck' column from display
     df_display = df.drop(columns=['uncheck'])
 
     df_display['isCarGood'] = False
@@ -50,23 +47,19 @@ def CarCheckerMain():
                     
                     if row['isCarGood']:
 
-                        # Write SQL query to add to greatcar table
                         cursor.execute(f"""
                             INSERT INTO greatcar (ucid)
                             VALUES ( {int(row['ucid'])} );
                         """)
         
                     else:
-                        # Placeholder for action when 'isCarGood' is False
                         st.write(f"ID {row['ucid']} is False. Do something else here.")
 
-                        # query to add to BrokenCar
                         cursor.execute(f"""
                             INSERT INTO brokencar (ucid)
                             VALUES ( {int(row['ucid'])});
                         """)
                         
-                # Updating all 'Uncheck' values to False
                 cursor.execute("UPDATE carunchecked SET uncheck = False WHERE uncheck = True;")
                 connection.commit()
 
@@ -74,7 +67,6 @@ def CarCheckerMain():
             st.error(f"Error: Unable to update the database. {e}")
 
         finally:
-            # Refetching the data after the update
             st.experimental_rerun()
 
 
